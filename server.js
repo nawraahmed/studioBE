a9fb1c06c23938f255f7e0b3f0b8cb8a4b67bb82
nawraahmed <nawra.alhaji@gmail.com>
2024-11-06 10:19:04 +0300

    the backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+//Load Dep
+const express = require('express')
+const mongoose = require('mongoose')
+
+//require and initalize dotenv
+require('dotenv').config()
+
+//PORT conf
+const PORT = process.env.PORT
+
+//Initalize express
+const app = express()
+
+//Database Configuration
+const db = require('./config/db')
+
+//listen on port
+app.listen(PORT, () => console.log(`running on port: ${PORT}`))
