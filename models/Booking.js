6105f8c2f05a4fb9452b8179ed1e2adff1a63e5b
Ayah Ahmed <ayahahmed729@gmail.com>
2024-11-06 15:14:07 +0300

    models fixing

--- models/Booking.js
+++ models/Booking.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const BookingSchema = new mongoose.Schema(
   {
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    service: { type: Schema.Types.ObjectId, ref: "Service", required: true },
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    service: { type: mongoose.Types.ObjectId, ref: "Service", required: true },
     bookingDate: { type: Date, required: true },
     status: {
       type: String,
--- models/BookingHistory.js
+++ models/BookingHistory.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const BookingHistorySchema = new mongoose.Schema(
   {
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    bookings: [{ type: Schema.Types.ObjectId, ref: "Booking" }],
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    bookings: [{ type: mongoose.Types.ObjectId, ref: "Booking" }],
   },
   { timestamps: true }
 )
