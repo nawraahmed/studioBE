b915fcb5408d868f46fe5a9172d5eebc3f807abc
FatemaJadeed <fatema.jadeed266@gmail.com>
2024-11-07 09:28:51 +0300

    JWT

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-//Load Dep
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require('express')
+const passport = require('passport')
+const mongoose = require('mongoose')
 
-//require and initalize dotenv
-require("dotenv").config()
-
-//PORT conf
-const PORT = process.env.PORT
+require('dotenv').config()
+require('./config/passport')
 
 //Initalize express
 const app = express()
 app.use(express.json())
+app.use(passport.initialize())
 
 //Database Configuration
-const db = require("./config/db")
+const db = require('./config/db')
 
 // routes
-const packageRoute = require("./routes/package")
-// Mount routes
-app.use("/package", packageRoute)
-//listen on port
+const authRoutes = require('./routes/auth')
+const packageRoute = require('./routes/package')
+
+app.use('/api', authRoutes)
+app.use('/package', packageRoute)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`running on port: ${PORT}`))
