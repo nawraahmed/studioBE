4794713b0ef2b06ac0190f706867860c72328df4
NoorAlJirdabi <noor.aljirdabi@gmail.com>
2024-11-11 10:05:07 +0300

    resolve conflicts

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -11,11 +11,7 @@ router.put(
   upload.array('files'),
   projectController.updateProject
 )
-<<<<<<< HEAD
+router.post('/project/:id/delete-file', projectController.deleteFile)
 router.delete('/project/:id', projectController.deleteProject)
-=======
-router.post("/project/:id/delete-file", projectController.deleteFile)
-router.delete("/project/:id", projectController.deleteProject)
->>>>>>> 73f10990c4c152069374ac345ac5e1b7f38864d3
 
 module.exports = router
