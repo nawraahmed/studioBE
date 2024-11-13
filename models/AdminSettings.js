199174c5b34e9ad862c0eac1d6751913a2e1d1f9
saraisa12 <saraisa.024@gmail.com>
2024-11-13 23:35:54 +0300

    service lang done

--- models/AdminSettings.js
+++ models/AdminSettings.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const AdminSettingsSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+      ref: "User",
+      required: true,
     },
     businessHours: { type: String, required: true },
     holidays: { type: [String], required: true },
-    unavailableDays: { type: [String], required: true }
+    unavailableDays: { type: [String], required: true },
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model('AdminSettings', AdminSettingsSchema)
+module.exports = mongoose.model("AdminSettings", AdminSettingsSchema)
